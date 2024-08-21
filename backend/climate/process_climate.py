6bf2763146e5af543477d46c9dd24cc6d4d44e39
eric <collinseric321@gmail.com>
2024-08-21 15:45:13 -0700

    more setup

---
+++ backend/climate/process_climate.py
@@ -0,0 +1,16 @@
+import xarray as xr
+import boto3
+
+
+def download_climate_data():
+    pass
+
+
+def main(s3_uris: str, file_format: str, crs: str) -> xr.Dataset:
+
+    
+    pass
+
+
+if __name__ == "__main__":
+    main()
