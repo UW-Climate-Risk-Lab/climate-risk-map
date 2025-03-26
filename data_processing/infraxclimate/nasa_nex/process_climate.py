e5eb662048498a810534cd97184e761771befb81
eric <collinseric321@gmail.com>
2025-03-26 10:58:07 -0700

    refactor infraxclimate

--- data_processing/infraxclimate/nasa_nex/process_climate.py
+++ data_processing/infraxclimate/nasa_nex/process_climate.py
@@ -17,94 +17,9 @@ logger = logging.getLogger(__name__)
 # INFO:botocore.httpchecksum:Skipping checksum validation. Response did not contain one of the following algorithms: ['crc32', 'sha1', 'sha256']
 logging.getLogger("botocore.httpchecksum").setLevel(logging.WARNING)
 
-
-def load_data(
-    s3_zarr_store_uri: str,
-    ssp: str,
-    climate_variable: str,
-    bbox: dict,
-) -> xr.DataArray:
-    """Reads all valid Zarr stores in the given S3 directory"""
-    data = []
-
-    fs = s3fs.S3FileSystem()
-    pattern = f"s3://{s3_bucket}/{s3_prefix}/*"
-    model_paths = fs.glob(pattern)
-
-
-    for model_path in model_paths:
-        model_name = model_path.rstrip("/").split("/")[-1]
-        logger.info(f"Validating model: {model_name}")
-
-        # Check if model has all required SSPs
-        if not validate_model_ssp(fs, model_path, ssp):
-            logger.warning(f"Skipping {model_name}: missing required SSP")
-            continue
-
-        # Get all zarr stores for this model and SSP
-        if ssp == '-999':
-            model_pattern = f"{model_path}/historical/*/{climate_variable}_day_*.zarr"
-        else:
-            model_pattern = f"{model_path}/ssp{ssp}/*/{climate_variable}_day_*.zarr"
-
-        zarr_stores = fs.glob(model_pattern)
-
-        # Check if model has all required years
-        if not validate_model_years(fs, zarr_stores):
-            logger.warning(f"Skipping {model_name}: missing required years")
-            continue
-
-        # Convert to full S3 URIs
-        zarr_uris = [f"s3://{path}" for path in zarr_stores]
-
-        logger.info(f"Loading validated model: {model_name}")
-        _ds = xr.open_mfdataset(
-            zarr_uris,
-            engine="zarr",
-            combine="by_coords",
-            parallel=True,
-            preprocess=decade_month_calc,
-        )
-        _da = _ds[climate_variable]
-        _da = _da.assign_coords({constants.X_DIM: (((_da[constants.X_DIM] + 180) % 360) - 180)})
-        _da = _da.sortby(constants.X_DIM)
-
-        # Bbox currently only in -180-180 lon
-        # TODO: Add better error and case handling
-        if bbox:
-            _da = _da.sel(
-                {
-                    constants.Y_DIM: slice(bbox["min_lat"], bbox["max_lat"]),
-                    constants.X_DIM: slice(bbox["min_lon"], bbox["max_lon"]),
-                },
-            )
-        _da = _da.assign_coords(model=model_name)
-        _da = _da.expand_dims("model")
-        data.append(_da)
-
-        logger.info(f"{model_name} loaded")
-    
-    da = xr.combine_nested(data, concat_dim=["model"])
-    da = da.assign_attrs(ensemble_members=da.model.values)
-
-    chunks = {
-        "decade_month": 12,  # All in one chunk since it's usually small
-        constants.X_DIM: "auto",
-        constants.Y_DIM: "auto",
-        "model": -1  # All models in one chunk for ensemble calculations
-    }
-
-    da = da.chunk(chunks)
-
-    return da
-
-
 def main(
     s3_zarr_store_uri: str,
-    climate_variable: str,
-    ssp: str,
     crs: str,
-    bbox: dict,
 ) -> xr.Dataset:
     """Processes climate data
 
@@ -120,25 +35,21 @@ def main(
         xr.Dataset: Xarray dataset of processed climate data
     """
 
-    ds = load_data(
-        s3_bucket=s3_bucket,
-        s3_prefix=s3_prefix,
-        ssp=ssp,
-        climate_variable=climate_variable,
-        bbox=bbox,
+    ds = xr.open_dataset(
+        s3_zarr_store_uri,
+        engine="zarr"
     )
 
-    logger.info("Xarray dataset created")
+    # Converts 0-360 longitude to -180-180 longitude. In line with OpenStreetMap database
+    ds = ds.assign_coords({constants.X_DIM: (((ds[constants.X_DIM] + 180) % 360) - 180)})
+    ds = ds.sortby(constants.X_DIM)
 
     # Sets the CRS based on the provided CRS
     ds.rio.write_crs(crs, inplace=True)
     ds.rio.set_spatial_dims(x_dim=constants.X_DIM, y_dim=constants.Y_DIM, inplace=True)
     ds.rio.write_coordinate_system(inplace=True)
 
-    
-
     return ds
-
 
 if __name__ == "__main__":
     main()
--- data_processing/infraxclimate/nasa_nex/run.py
+++ data_processing/infraxclimate/nasa_nex/run.py
@@ -1,12 +1,23 @@
 import argparse
 import logging
+import os
 
-import pipeline
+from psycopg2 import pool
+
+import infra_intersection
+import infra_intersection_load
+import process_climate
+import utils
 import constants
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+PG_DBNAME = os.environ["PG_DBNAME"]
+PG_USER = os.environ["PG_USER"]
+PG_PASSWORD = os.environ["PG_PASSWORD"]
+PG_HOST = os.environ["PG_HOST"]
+PG_PORT = os.environ["PG_PORT"]
 
 def setup_args():
     parser = argparse.ArgumentParser(description="Process climate data for a given SSP")
@@ -20,23 +31,79 @@ def setup_args():
     parser.add_argument(
         "--zonal-agg-method", required=True, help="Zonal aggregation method"
     )
-    parser.add_argument("--state-bbox", help="State bounding box", required=False)
     parser.add_argument("--osm-category", required=True, help="OSM category")
     parser.add_argument("--osm-type", required=True, help="OSM type")
     return parser.parse_args()
 
 
+def main(
+    s3_zarr_store_uri: str,
+    climate_variable: str,
+    ssp: str,
+    crs: str,
+    zonal_agg_method: str,
+    osm_category: str,
+    osm_type: str,
+):
+    """Runs a processing pipeline for a given zarr store"""
+
+    # Create connection pool with passed parameters
+    connection_pool = pool.SimpleConnectionPool(
+        minconn=1,
+        maxconn=3,
+        dbname=PG_DBNAME,
+        user=PG_USER,
+        password=PG_PASSWORD,
+        host=PG_HOST,
+        port=PG_PORT,
+    )
+
+    ds = process_climate.main(
+        s3_zarr_store_uri=s3_zarr_store_uri,
+        crs=crs,
+    )
+
+    metadata = utils.create_metadata(
+        ds=ds
+    )
+
+    metadata[constants.METADATA_KEY]["zonal_agg_method"] = zonal_agg_method
+
+    logger.info("Climate Data Loaded & Processed")
+
+    infra_intersection_conn = connection_pool.getconn()
+    df = infra_intersection.main(
+        climate_ds=ds,
+        osm_category=osm_category,
+        osm_type=osm_type,
+        crs=crs,
+        zonal_agg_method=zonal_agg_method,
+        conn=infra_intersection_conn,
+        metadata=metadata
+    )
+    connection_pool.putconn(infra_intersection_conn)
+    logger.info("Infrastructure Intersection Complete")
+
+    infra_intersection_load_conn = connection_pool.getconn()
+    infra_intersection_load.main(
+        df=df,
+        ssp=ssp,
+        climate_variable=climate_variable,
+        conn=infra_intersection_load_conn,
+    )
+    connection_pool.putconn(infra_intersection_load_conn)
+
+
 if __name__ == "__main__":
     args = setup_args()
-    logger.info(f"STARTING PIPELINE FOR {args.s3_zarr_store_uri}")
-    pipeline.main(
+    logger.info(f"STARTING EXPOSURE CALCULATION FOR {args.s3_zarr_store_uri}")
+    main(
         s3_zarr_store_uri=args.s3_zarr_store_uri,
         climate_variable=args.climate_variable,
         ssp=args.ssp,
         crs=args.crs,
         zonal_agg_method=args.zonal_agg_method,
-        state_bbox=args.state_bbox,
         osm_category=args.osm_category,
         osm_type=args.osm_type,
     )
-    logger.info(f"PIPELINE SUCCEEDED FOR {args.s3_zarr_store_uri}")
+    logger.info(f"EXPOSURE SUCCEEDED FOR {args.s3_zarr_store_uri}")
