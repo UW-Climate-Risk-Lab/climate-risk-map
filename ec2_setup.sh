8b0975fd0697a3be5cb8aff0be7c0f0d8916c011
eric <collinseric321@gmail.com>
2025-03-28 17:29:16 -0700

    refactor ec2_setup.sh to install Miniconda for the ubuntu user and add AWS CLI installation

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -7,16 +7,6 @@ sudo apt-get upgrade -y
 sudo apt-get install -y git
 sudo apt-get install -y gh
 
-
-# Install Miniconda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
-bash /tmp/miniconda.sh -b -p $HOME/miniconda
-rm /tmp/miniconda.sh
-# Configure Miniconda
-$HOME/miniconda/bin/conda init bash
-echo 'export PATH="/home/ubuntu/miniconda/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-
 # Install Docker
 sudo apt-get install -y docker.io
 sudo systemctl start docker
@@ -33,3 +23,16 @@ sudo apt-get install -y gdal-bin libgdal-dev python3-gdal
 # Install additional Python libraries that will be useful
 sudo apt-get install -y python3-pip
 
+# Install Miniconda for the ubuntu user
+su - ubuntu -c "wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh"
+su - ubuntu -c "bash /tmp/miniconda.sh -b -p /home/ubuntu/miniconda"
+rm /tmp/miniconda.sh
+
+# Initialize conda in the user's bashrc
+su - ubuntu -c "/home/ubuntu/miniconda/bin/conda init bash"
+
+# Install AWS CLI
+sudo apt-get install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
